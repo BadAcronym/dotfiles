0706f42f87dd50d19ecc70fce5052b535899c89e
BadAcronym <terribleacronym@gmail.com>
2026-04-18 12:01:33 +0200

    add text width, change keybinds

--- nvim/lua/nvimoptions.lua
+++ nvim/lua/nvimoptions.lua
@@ -1,7 +1,7 @@
-vim.wo.wrap = false
-vim.wo.number = true
+vim.wo.wrap           = false
+vim.wo.number         = true
 vim.wo.relativenumber = true
-vim.wo.signcolumn = 'yes:1'
+vim.wo.signcolumn     = 'yes:1'
 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
@@ -12,6 +12,10 @@ vim.opt.clipboard     = "unnamedplus"
 vim.opt.guicursor     = "n-v-c-i:block-Cursor/lcursor"
 vim.opt.cursorline    = true
 vim.opt.cursorlineopt = "line"
+-- perfect size for two split terminals, side-by-side with my current wezterm config!
+vim.opt.textwidth     = 88
+-- always 1 more than textwidth.
+vim.opt.colorcolumn   = "89"
 
 vim.o.swapfile    = false
 vim.o.undofile    = true
@@ -22,8 +26,8 @@ vim.o.undodir     = os.getenv("HOME") .. "/.cache/nvim/undodir"
 vim.opt.fillchars = "eob: "
 
 vim.keymap.set({"n", "v"}, "<C-h>", "<C-u>")  --move
-vim.keymap.set({"n", "v"}, "f", "<C-r>")      --redo
+vim.keymap.set({"n", "v"}, "r", "<C-r>")      --redo
+vim.keymap.set({"n", "v"}, "<leader>r", "q")  --record macro
 vim.keymap.set({"n", "v"}, "q", "<cmd>q<CR>") --quit buffer
-vim.keymap.set({"n", "v"}, "<leader>r", "qq") --record macro
 
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
