24e292d441af6a8279bd2fc0d1f74bdee205b0b4
BadAcronym <terribleacronym@gmail.com>
2026-04-18 11:39:58 +0200

    switch to main

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -1,7 +1,5 @@
 return {
-    --NOTE: fixes have not been merged into master yet
-    -- "nvimdev/lspsaga.nvim",
-    "leeguooooo/lspsaga.nvim",
+    "nvimdev/lspsaga.nvim",
     keys = {
         { "go", "<cmd>Lspsaga goto_definition<CR>",         mode = "n", noremap = true, silent = true },
         { "gh", "<cmd>Lspsaga finder<CR>",                  mode = "n", noremap = true, silent = true },
