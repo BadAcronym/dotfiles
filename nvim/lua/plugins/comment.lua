e5f7b1aaea619298739875a1cb5f5c828ac7e1fd
BadAcronym <terribleacronym@gmail.com>
2026-04-18 11:39:47 +0200

    lint

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -5,26 +5,26 @@ return {
 		event = { "BufRead", "BufNewFile" },
 		opts = {
 			padding = true,
-			sticky = true,
-			ignore = nil,
+			sticky  = true,
+			ignore  = nil,
 			toggler = {
-				line = "gcc",
+				line  = "gcc",
 				block = "gbc",
 			},
 			opleader = {
-				line = "gc",
+				line  = "gc",
 				block = "gb",
 			},
 			extra = {
 				above = "gcO",
 				below = "gco",
-				eol = "gcA",
+				eol   = "gcA",
 			},
 			mappings = {
 				basic = true,
 				extra = true,
 			},
-			pre_hook = nil,
+			pre_hook  = nil,
 			post_hook = nil,
 		},
 		config = function()
